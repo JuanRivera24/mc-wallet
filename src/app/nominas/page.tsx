c115c91d6d56edc6180672064e168e597d6204a2
JuanRivera24 <juanmrivera0424@gmail.com>
2026-02-20 08:49:29 -0500

    Actualizar page.tsx

--- src/app/nominas/page.tsx
+++ src/app/nominas/page.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useMemo } from "react";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import { useUser, SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
 import { db } from "@/lib/firebase";
 import { collection, query, where, onSnapshot, doc, deleteDoc, setDoc, serverTimestamp } from "firebase/firestore";
